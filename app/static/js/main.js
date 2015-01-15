317436c0d62efdb5a2781f5bd5afc14802cd06ac
Urim Choi <uchoi@uchoi.local>
2015-01-15 15:42:56 -0500

    dynamic list, title format, chart border

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -9,7 +9,13 @@ $(document).ready(function() {
 
 */
 
-	console.log('ready');
+	var campaign_id_list = [1192, 1994, 1050];
+
+	for ( i = 0; i < campaign_id_list.length; i++ ) {
+		var link = "<a href='/campaigns/" + campaign_id_list[i] + "'><li>" + campaign_id_list[i] + "</li></a>";
+		$('#side_nav ul').append(link);
+		console.log(link);
+	}
 
 	$("li").each(function(){
 		$(this).hover(function(){
